c99f24154f2ecb6e1b1a29d0d7d31048f9c821ec
jon-mah <jonmah@uw.edu>
2019-07-11 11:44:54 -0700

    Pipeline through step 3.5

--- Scripts/pipeline.bash
+++ Scripts/pipeline.bash
@@ -1,13 +1,17 @@
 # qsub -cwd -V -N job_name pipeline.bash
+
+# INPUT ARGUMENTS
 num_ind_1=3
 num_ind_2=2
-outdir='../Data/pilot_pipeline/'
-outprefix='${outdir}seed_1_two_pop'
-outputvcf='${outprefix}.vcf'
-output_pops_file='${outprefix}_pops_file.txt'
-easySFS_outdir='${outdir}/easySFS/'
-easySFS_proj = '${num_ind_1},${num_ind_2}'
+outdir="../Data/pilot_pipeline/"
+outprefix="${outdir}seed_1_two_pop"
 
-slim pipeline.slim
+# DERIVED ARGUMENTS
+inputvcf="${outprefix}.vcf"
+output_pops_file="${outprefix}_pops_file.txt"
+easySFS_outdir="${outdir}/easySFS_output/"
+easySFS_proj="${num_ind_1},${num_ind_2}"
+
+# slim slim_pipeline.slim
 python write_pops_file.py $num_ind_1 $num_ind_2 $outprefix
-easySFS.py -i $outputvcf -p $output_pops_file -o $easySFS_outdir -f -a --proj $easySFS_proj
+easySFS.py -i $inputvcf -p $output_pops_file -o $easySFS_outdir -f -a --proj $easySFS_proj
