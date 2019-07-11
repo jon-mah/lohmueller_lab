4c4413b6e3b277df01fd456914a5930304dc4f98
Jonathan Mah <jonmah@uw.edu>
2019-07-11 10:28:43 -0700

    File cleanup, pipeline.slim

--- Scripts/pipeline.bash
+++ Scripts/pipeline.bash
@@ -6,9 +6,8 @@ outprefix='${outdir}seed_1_two_pop'
 outputvcf='${outprefix}.vcf'
 output_pops_file='${outprefix}_pops_file.txt'
 easySFS_outdir='${outdir}/easySFS/'
-easySFS_proj = '${num_ind_1},${num_ind_2}
+easySFS_proj = '${num_ind_1},${num_ind_2}'
 
-slim sim_multi_pop_one_vcf.slim
+slim pipeline.slim
 python write_pops_file.py $num_ind_1 $num_ind_2 $outprefix
 easySFS.py -i $outputvcf -p $output_pops_file -o $easySFS_outdir -f -a --proj $easySFS_proj
-
