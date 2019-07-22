0e028ac270a849acef543d4ef6580545d334c82c
jon-mah <jonmah@uw.edu>
2019-07-22 13:30:24 -0700

    Cluster outputs.

--- Scripts/AW_recap_1.bash
+++ Scripts/AW_recap_1.bash
@@ -3,7 +3,7 @@
 #$ -V
 #$ -m bea
 #$ -l h_data=5G
-#$ -l h_rt=24:00:00
+#$ -l h_rt=12:00:00
 
 # INPUT ARGUMENTS
 num_ind=15 # Number of samples from single population.
@@ -20,9 +20,6 @@ syn_easySFS_outdir="${prefix}_easySFS_output_syn/"
 nonsyn_easySFS_outdir="${prefix}_easySFS_output_nonsyn/"
 num_samples=$(($num_ind * 2))
 
-conda deactivate
-conda activate jonmah_env_3
-
 slim AW_recap_trees.slim # Run slim file to generate `.trees`.
 python AW_recap_python.py # Run python file to recapitate.
 slim AW_recap_vcf.slim # Run slim file to generate `.vcf`.
@@ -31,16 +28,13 @@ slim AW_recap_vcf.slim # Run slim file to generate `.vcf`.
 grep "#\|;MT=1" ${prefix}.vcf > $syn_inputvcf
 grep "#\|;MT=2" ${prefix}.vcf > $nonsyn_inputvcf
 
-conda deactivate
-conda activate jonmah_env
-
 # Write pops_file.txt for easySFS.py.
 python write_pops_file.py $num_ind 0 ${prefix}_syn
 python write_pops_file.py $num_ind 0 ${prefix}_nonsyn
 
 # Use easySFS.py to compute synonymous and nonsynonymous sfs.
-python easySFS.py -i $syn_inputvcf -p $syn_output_pops_file -o $syn_easySFS_outdir -f -a --proj $easySFS_proj
-python easySFS.py -i $nonsyn_inputvcf -p $nonsyn_output_pops_file -o $nonsyn_easySFS_outdir -f -a --proj $easySFS_proj
+# python easySFS.py -i $syn_inputvcf -p $syn_output_pops_file -o $syn_easySFS_outdir -f -a --proj $easySFS_proj
+# python easySFS.py -i $nonsyn_inputvcf -p $nonsyn_output_pops_file -o $nonsyn_easySFS_outdir -f -a --proj $easySFS_proj
 
 # Use fitdadi_infer_DFE.py to infer DFE for given synonymous and nonsynonymous sfs.
-python fitdadi_infer_DFE.py ${syn_easySFS_outdir}dadi/pop1.sfs ${nonsyn_easySFS_outdir}dadi/pop1.sfs ${prefix}_fitdadi_output/ --breed "AW"
+# python fitdadi_infer_DFE.py ${syn_easySFS_outdir}dadi/pop1.sfs ${nonsyn_easySFS_outdir}dadi/pop1.sfs ${prefix}_fitdadi_output/ --breed "AW"
