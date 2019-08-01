69b644400a0f7df94d75433b74ddffc799c0faf1
Jonathan Mah <jonmah@uw.edu>
2019-08-01 12:33:19 -0700

    pipeline temp change/

---
+++ Scripts/plot_sfs_residual.R
@@ -0,0 +1,120 @@
+# plot_sfs_residual.R
+# Author: Jonathan Mah
+# Date: 20190731
+# This script is used to visually compare the SFS of model and data.
+
+library(ggplot2)
+library(reshape2)
+
+setwd("C:/Users/jonat/Desktop/UCLA_BIG_SURP/lohmueller_lab")
+
+input_nonsyn_data = as.numeric(unlist(strsplit(readLines('Eduardo_Scripts/20190715/ns_SFS/AW.sfs')[2], ' ')))
+seed_1_nonsyn_data = as.numeric(unlist(strsplit(readLines('Data/AW_array_length_20154/seed_1_easySFS_output_nonsyn/dadi/pop1.sfs')[2], ' ')))
+seed_2_nonsyn_data = as.numeric(unlist(strsplit(readLines('Data/AW_array_length_20154_ind_15/seed_1_easySFS_output_nonsyn/dadi/pop1.sfs')[2], ' ')))
+
+input_syn_data = as.numeric(unlist(strsplit(readLines('Eduardo_Scripts/20190715/syn_SFS/AW.sfs')[2], ' ')))
+seed_1_syn_data = as.numeric(unlist(strsplit(readLines('Data/AW_array_length_20154/seed_1_easySFS_output_syn/dadi/pop1.sfs')[2], ' ')))
+seed_2_syn_data = as.numeric(unlist(strsplit(readLines('Data/AW_array_length_20154_ind_15/seed_1_easySFS_output_syn/dadi/pop1.sfs')[2], ' ')))
+
+x_axis = 1:17
+
+nonsynonymous_df_1 = data.frame(input = input_nonsyn_data,
+                              model = seed_1_nonsyn_data,
+                              x_axis = x_axis)
+
+synonymous_df_1 = data.frame(input = input_syn_data,
+                           model = seed_1_syn_data,
+                           x_axis = x_axis)
+
+p_syn_1 <- ggplot(data=synonymous_df_1, aes(x=x_axis, y=input, color='input')) +
+  geom_point() +
+  geom_line() +
+  geom_point(aes(x=x_axis, y=model, color = 'model')) + 
+  geom_line(aes(x=x_axis, y=model, color = 'model')) +
+  xlab('Number of sites') +
+  ylab('Site frequency') +
+  ggtitle('Residual for Synonymous Site Frequency Spectrum, seed=1') +
+  scale_color_manual(values=c("blue", "red"), 
+                    name="Data Type",
+                    breaks=c("input", "model"),
+                    labels=c("Real data", "Simulated data"))
+p_syn_1
+
+p_nonsyn_1 <- ggplot(data=nonsynonymous_df_1, aes(x=x_axis, y=input, color='input')) +
+  geom_point() +
+  geom_line() +
+  geom_point(aes(x=x_axis, y=model, color = 'model')) + 
+  geom_line(aes(x=x_axis, y=model, color = 'model')) +
+  xlab('Number of sites') +
+  ylab('Site frequency') +
+  ggtitle('Residual for Nonsynonymous Site Frequency Spectrum, seed=1') +
+  scale_color_manual(values=c("blue", "red"), 
+                     name="Data Type",
+                     breaks=c("input", "model"),
+                     labels=c("Real data", "Simulated data"))
+p_nonsyn_1
+
+x_axis = x_axis
+
+nonsynonymous_df_2 = data.frame(input = input_nonsyn_data,
+                                model = seed_2_nonsyn_data,
+                                x_axis = x_axis)
+
+synonymous_df_2 = data.frame(input = input_syn_data,
+                             model = seed_2_syn_data,
+                             x_axis = x_axis)
+
+p_syn_2 <- ggplot(data=synonymous_df_2, aes(x=x_axis, y=input, color='input')) +
+  geom_point() +
+  geom_line() +
+  geom_point(aes(x=x_axis, y=model, color = 'model')) + 
+  geom_line(aes(x=x_axis, y=model, color = 'model')) +
+  xlab('Number of sites') +
+  ylab('Site frequency') +
+  ggtitle('Residual for Synonymous Site Frequency Spectrum, seed=2') +
+  scale_color_manual(values=c("blue", "red"), 
+                     name="Data Type",
+                     breaks=c("input", "model"),
+                     labels=c("Real data", "Simulated data"))
+p_syn_2
+
+p_nonsyn_2 <- ggplot(data=nonsynonymous_df_2, aes(x=x_axis, y=input, color='input')) +
+  geom_point() +
+  geom_line() +
+  geom_point(aes(x=x_axis, y=model, color = 'model')) + 
+  geom_line(aes(x=x_axis, y=model, color = 'model')) +
+  xlab('Number of sites') +
+  ylab('Site frequency') +
+  ggtitle('Residual for Nonsynonymous Site Frequency Spectrum, seed=2') +
+  scale_color_manual(values=c("blue", "red"), 
+                     name="Data Type",
+                     breaks=c("input", "model"),
+                     labels=c("Real data", "Simulated data"))
+p_nonsyn_2
+
+
+
+
+
+# Scratch
+seed_vector = 1:100
+shape_vector = c()
+scale_vector = c()
+for (i in 1:100) {
+  file = gsub('FILENUM', i, 'Data/AW_array_2000/seed_FILENUM_fitdadi_output/inferred_DFE.txt')
+  file = readLines(file)
+  params = file[2]
+  params = unlist(strsplit(params, 'array'))[2]
+  shape = unlist(strsplit(params, ' '))[1]
+  shape = substr(shape, start = 3, stop = nchar(shape))
+  scale = unlist(strsplit(params, shape, fixed=TRUE))[2]
+  scale = substr(scale, start = 1, stop = nchar(scale) - 4)
+  shape_vector = c(shape_vector, shape)
+  scale_vector = c(scale_vector, scale)
+}
+
+Na_2000_data = data.frame(seed=seed_vector, shape=as.numeric(shape_vector), scale=as.numeric(scale_vector))
+write.csv(Na_2000_data, 'Data/AW_2000_parameters.csv', col.names=TRUE, row.names=FALSE)
+
+mean(as.numeric(shape_vector))
+mean(as.numeric(scale_vector))
