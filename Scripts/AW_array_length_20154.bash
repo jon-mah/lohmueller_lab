6e818939467aaafe63027349127c613272a436f0
jon-mah <jonmah@uw.edu>
2019-08-12 08:25:14 -0700

    pipeline outputs.

--- Scripts/AW_array_length_20154.bash
+++ Scripts/AW_array_length_20154.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-#$ -t 52
+#$ -t 634
 #$ -cwd
 #$ -V
 #$ -m a
@@ -7,7 +7,7 @@
 #$ -l h_rt=23:55:00
 
 # INPUT ARGUMENTS
-seed=1
+seed=5
 num_ind=8 # Number of samples from single population.
 prefix="../Data/AW_array_length_20154/seed_${seed}" # Output prefix, and input prefix of given vcf
 easySFS_proj=16 # Number of chromosomes that sample is projected down into.
