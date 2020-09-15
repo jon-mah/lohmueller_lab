b8c0a3d3fdd18fc35c89d5d5ed2cb4cb0037ad16
jon-mah <jonmah@uw.edu>
2020-09-15 09:53:05 -0700

    DFE inference for gen 350.

--- Scripts/pop_4000_gen_350_DFE_inference.bash
+++ Scripts/pop_4000_gen_350_DFE_inference.bash
@@ -30,7 +30,7 @@ python write_pops_file.py $num_ind 0 ${prefix}_nonsyn
 python easySFS.py -i $syn_inputvcf -p $syn_output_pops_file -o $syn_easySFS_outdir -f -a --proj $easySFS_proj
 python easySFS.py -i $nonsyn_inputvcf -p $nonsyn_output_pops_file -o $nonsyn_easySFS_outdir -f -a --proj $easySFS_proj
 # Use fitdadi_infer_DFE.py to infer DFE for given synonymous and nonsynonymous sfs.
-python fitdadi_infer_DFE.py ${syn_easySFS_outdir}dadi/pop1.sfs ${nonsyn_easySFS_outdir}dadi/pop1.sfs ${prefix}_fitdadi_output/ --breed "LB"
+# python fitdadi_infer_DFE.py ${syn_easySFS_outdir}dadi/pop1.sfs ${nonsyn_easySFS_outdir}dadi/pop1.sfs ${prefix}_fitdadi_output/ --breed "LB"
 
 # INPUT ARGUMENTS
 prefix="../Data/pop_4000_neutral/350" # Output prefix, and input prefix of given vcf
@@ -55,7 +55,7 @@ python write_pops_file.py $num_ind 0 ${prefix}_nonsyn
 python easySFS.py -i $syn_inputvcf -p $syn_output_pops_file -o $syn_easySFS_outdir -f -a --proj $easySFS_proj
 python easySFS.py -i $nonsyn_inputvcf -p $nonsyn_output_pops_file -o $nonsyn_easySFS_outdir -f -a --proj $easySFS_proj
 # Use fitdadi_infer_DFE.py to infer DFE for given synonymous and nonsynonymous sfs.
-python fitdadi_infer_DFE.py ${syn_easySFS_outdir}dadi/pop1.sfs ${nonsyn_easySFS_outdir}dadi/pop1.sfs ${prefix}_fitdadi_output/ --breed "LB"
+# python fitdadi_infer_DFE.py ${syn_easySFS_outdir}dadi/pop1.sfs ${nonsyn_easySFS_outdir}dadi/pop1.sfs ${prefix}_fitdadi_output/ --breed "LB"
 
 
 # INPUT ARGUMENTS
@@ -81,7 +81,7 @@ python write_pops_file.py $num_ind 0 ${prefix}_nonsyn
 python easySFS.py -i $syn_inputvcf -p $syn_output_pops_file -o $syn_easySFS_outdir -f -a --proj $easySFS_proj
 python easySFS.py -i $nonsyn_inputvcf -p $nonsyn_output_pops_file -o $nonsyn_easySFS_outdir -f -a --proj $easySFS_proj
 # Use fitdadi_infer_DFE.py to infer DFE for given synonymous and nonsynonymous sfs.
-python fitdadi_infer_DFE.py ${syn_easySFS_outdir}dadi/pop1.sfs ${nonsyn_easySFS_outdir}dadi/pop1.sfs ${prefix}_fitdadi_output/ --breed "LB"
+# python fitdadi_infer_DFE.py ${syn_easySFS_outdir}dadi/pop1.sfs ${nonsyn_easySFS_outdir}dadi/pop1.sfs ${prefix}_fitdadi_output/ --breed "LB"
 
 
 # INPUT ARGUMENTS
@@ -107,7 +107,7 @@ python write_pops_file.py $num_ind 0 ${prefix}_nonsyn
 python easySFS.py -i $syn_inputvcf -p $syn_output_pops_file -o $syn_easySFS_outdir -f -a --proj $easySFS_proj
 python easySFS.py -i $nonsyn_inputvcf -p $nonsyn_output_pops_file -o $nonsyn_easySFS_outdir -f -a --proj $easySFS_proj
 # Use fitdadi_infer_DFE.py to infer DFE for given synonymous and nonsynonymous sfs.
-python fitdadi_infer_DFE.py ${syn_easySFS_outdir}dadi/pop1.sfs ${nonsyn_easySFS_outdir}dadi/pop1.sfs ${prefix}_fitdadi_output/ --breed "LB"
+# python fitdadi_infer_DFE.py ${syn_easySFS_outdir}dadi/pop1.sfs ${nonsyn_easySFS_outdir}dadi/pop1.sfs ${prefix}_fitdadi_output/ --breed "LB"
 
 
 # INPUT ARGUMENTS
@@ -133,7 +133,7 @@ python write_pops_file.py $num_ind 0 ${prefix}_nonsyn
 python easySFS.py -i $syn_inputvcf -p $syn_output_pops_file -o $syn_easySFS_outdir -f -a --proj $easySFS_proj
 python easySFS.py -i $nonsyn_inputvcf -p $nonsyn_output_pops_file -o $nonsyn_easySFS_outdir -f -a --proj $easySFS_proj
 # Use fitdadi_infer_DFE.py to infer DFE for given synonymous and nonsynonymous sfs.
-python fitdadi_infer_DFE.py ${syn_easySFS_outdir}dadi/pop1.sfs ${nonsyn_easySFS_outdir}dadi/pop1.sfs ${prefix}_fitdadi_output/ --breed "LB"
+# python fitdadi_infer_DFE.py ${syn_easySFS_outdir}dadi/pop1.sfs ${nonsyn_easySFS_outdir}dadi/pop1.sfs ${prefix}_fitdadi_output/ --breed "LB"
 
 
 # INPUT ARGUMENTS
@@ -159,4 +159,30 @@ python write_pops_file.py $num_ind 0 ${prefix}_nonsyn
 python easySFS.py -i $syn_inputvcf -p $syn_output_pops_file -o $syn_easySFS_outdir -f -a --proj $easySFS_proj
 python easySFS.py -i $nonsyn_inputvcf -p $nonsyn_output_pops_file -o $nonsyn_easySFS_outdir -f -a --proj $easySFS_proj
 # Use fitdadi_infer_DFE.py to infer DFE for given synonymous and nonsynonymous sfs.
+# python fitdadi_infer_DFE.py ${syn_easySFS_outdir}dadi/pop1.sfs ${nonsyn_easySFS_outdir}dadi/pop1.sfs ${prefix}_fitdadi_output/ --breed "LB"
+
+# INPUT ARGUMENTS
+prefix="../Data/pop_4000_no_demo_shift/350" # Output prefix, and input prefix of given vcf
+
+# DERIVED ARGUMENTS
+inputvcf="${prefix}.vcf"
+syn_inputvcf="${prefix}_syn.vcf"
+nonsyn_inputvcf="${prefix}_nonsyn.vcf"
+syn_output_pops_file="${prefix}_syn_pops_file.txt"
+nonsyn_output_pops_file="${prefix}_nonsyn_pops_file.txt"
+syn_easySFS_outdir="${prefix}_easySFS_output_syn/"
+nonsyn_easySFS_outdir="${prefix}_easySFS_output_nonsyn/"
+num_samples=$(($num_ind * 2))
+
+# Separate input `.vcf` file into synonymous and nonsynonymous.
+grep "#\|;MT=1" ${prefix}.vcf > $syn_inputvcf
+grep "#\|;MT=2" ${prefix}.vcf > $nonsyn_inputvcf
+# Write pops_file.txt for easySFS.py.
+python write_pops_file.py $num_ind 0 ${prefix}_syn
+python write_pops_file.py $num_ind 0 ${prefix}_nonsyn
+# Use easySFS.py to compute synonymous and nonsynonymous sfs.
+python easySFS.py -i $syn_inputvcf -p $syn_output_pops_file -o $syn_easySFS_outdir -f -a --proj $easySFS_proj
+python easySFS.py -i $nonsyn_inputvcf -p $nonsyn_output_pops_file -o $nonsyn_easySFS_outdir -f -a --proj $easySFS_proj
+# Use fitdadi_infer_DFE.py to infer DFE for given synonymous and nonsynonymous sfs.
 python fitdadi_infer_DFE.py ${syn_easySFS_outdir}dadi/pop1.sfs ${nonsyn_easySFS_outdir}dadi/pop1.sfs ${prefix}_fitdadi_output/ --breed "LB"
+
