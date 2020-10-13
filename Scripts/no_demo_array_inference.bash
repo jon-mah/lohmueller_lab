36f1dfaf5dfd21663bb1725376f1e83852fd88fc
Jonathan Mah <jonmah@uw.edu>
2020-10-13 08:40:01 -0700

    Simulation scripts.

--- Scripts/no_demo_array_inference.bash
+++ Scripts/no_demo_array_inference.bash
@@ -42,4 +42,214 @@ python write_pops_file.py $num_ind 0 ${prefix}_nonsyn
 python easySFS.py -i $syn_inputvcf -p $syn_output_pops_file -o $syn_easySFS_outdir -f -a --proj $easySFS_proj
 python easySFS.py -i $nonsyn_inputvcf -p $nonsyn_output_pops_file -o $nonsyn_easySFS_outdir -f -a --proj $easySFS_proj
 # Use fitdadi_infer_DFE.py to infer DFE for given synonymous and nonsynonymous sfs.
-python fitdadi_infer_DFE.py ${syn_easySFS_outdir}dadi/pop1.sfs ${nonsyn_easySFS_outdir}dadi/pop1.sfs ${prefix}_fitdadi_output/ --breed "LB"
+# python fitdadi_infer_DFE.py ${syn_easySFS_outdir}dadi/pop1.sfs ${nonsyn_easySFS_outdir}dadi/pop1.sfs ${prefix}_fitdadi_output/ --breed "LB"
+
+# INPUT ARGUMENTS
+prefix="../Data/no_demo_array/50_seed_${seed}" # Output prefix, and input prefix of given vcf
+
+# DERIVED ARGUMENTS
+inputvcf="${prefix}.vcf"
+syn_inputvcf="${prefix}_syn.vcf"
+nonsyn_inputvcf="${prefix}_nonsyn.vcf"
+syn_output_pops_file="${prefix}_syn_pops_file.txt"
+nonsyn_output_pops_file="${prefix}_nonsyn_pops_file.txt"
+syn_easySFS_outdir="${prefix}_easySFS_output_syn/"
+nonsyn_easySFS_outdir="${prefix}_easySFS_output_nonsyn/"
+num_samples=$(($num_ind * 2))
+
+# Merge all `.vcf` files.
+
+cp ${prefix}_chrom_1.vcf ${prefix}.vcf
+
+for i in {2..80}
+do
+  sed -i -r "s/^1\t/${i}\t/g" ${prefix}_chrom_${i}.vcf
+  grep ";MT=1\|;MT=2" ${prefix}_chrom_${i}.vcf >> ${prefix}.vcf
+done
+
+# Separate input `.vcf` file into synonymous and nonsynonymous.
+grep "#\|;MT=1" ${prefix}.vcf > $syn_inputvcf
+grep "#\|;MT=2" ${prefix}.vcf > $nonsyn_inputvcf
+# Write pops_file.txt for easySFS.py.
+python write_pops_file.py $num_ind 0 ${prefix}_syn
+python write_pops_file.py $num_ind 0 ${prefix}_nonsyn
+# Use easySFS.py to compute synonymous and nonsynonymous sfs.
+python easySFS.py -i $syn_inputvcf -p $syn_output_pops_file -o $syn_easySFS_outdir -f -a --proj $easySFS_proj
+python easySFS.py -i $nonsyn_inputvcf -p $nonsyn_output_pops_file -o $nonsyn_easySFS_outdir -f -a --proj $easySFS_proj
+# Use fitdadi_infer_DFE.py to infer DFE for given synonymous and nonsynonymous sfs.
+# python fitdadi_infer_DFE.py ${syn_easySFS_outdir}dadi/pop1.sfs ${nonsyn_easySFS_outdir}dadi/pop1.sfs ${prefix}_fitdadi_output/ --breed "LB"
+
+# INPUT ARGUMENTS
+prefix="../Data/no_demo_array/100_seed_${seed}" # Output prefix, and input prefix of given vcf
+
+# DERIVED ARGUMENTS
+inputvcf="${prefix}.vcf"
+syn_inputvcf="${prefix}_syn.vcf"
+nonsyn_inputvcf="${prefix}_nonsyn.vcf"
+syn_output_pops_file="${prefix}_syn_pops_file.txt"
+nonsyn_output_pops_file="${prefix}_nonsyn_pops_file.txt"
+syn_easySFS_outdir="${prefix}_easySFS_output_syn/"
+nonsyn_easySFS_outdir="${prefix}_easySFS_output_nonsyn/"
+num_samples=$(($num_ind * 2))
+
+# Merge all `.vcf` files.
+
+cp ${prefix}_chrom_1.vcf ${prefix}.vcf
+
+for i in {2..80}
+do
+  sed -i -r "s/^1\t/${i}\t/g" ${prefix}_chrom_${i}.vcf
+  grep ";MT=1\|;MT=2" ${prefix}_chrom_${i}.vcf >> ${prefix}.vcf
+done
+
+# Separate input `.vcf` file into synonymous and nonsynonymous.
+grep "#\|;MT=1" ${prefix}.vcf > $syn_inputvcf
+grep "#\|;MT=2" ${prefix}.vcf > $nonsyn_inputvcf
+# Write pops_file.txt for easySFS.py.
+python write_pops_file.py $num_ind 0 ${prefix}_syn
+python write_pops_file.py $num_ind 0 ${prefix}_nonsyn
+# Use easySFS.py to compute synonymous and nonsynonymous sfs.
+python easySFS.py -i $syn_inputvcf -p $syn_output_pops_file -o $syn_easySFS_outdir -f -a --proj $easySFS_proj
+python easySFS.py -i $nonsyn_inputvcf -p $nonsyn_output_pops_file -o $nonsyn_easySFS_outdir -f -a --proj $easySFS_proj
+# Use fitdadi_infer_DFE.py to infer DFE for given synonymous and nonsynonymous sfs.
+# python fitdadi_infer_DFE.py ${syn_easySFS_outdir}dadi/pop1.sfs ${nonsyn_easySFS_outdir}dadi/pop1.sfs ${prefix}_fitdadi_output/ --breed "LB"
+
+# INPUT ARGUMENTS
+prefix="../Data/no_demo_array/150_seed_${seed}" # Output prefix, and input prefix of given vcf
+
+# DERIVED ARGUMENTS
+inputvcf="${prefix}.vcf"
+syn_inputvcf="${prefix}_syn.vcf"
+nonsyn_inputvcf="${prefix}_nonsyn.vcf"
+syn_output_pops_file="${prefix}_syn_pops_file.txt"
+nonsyn_output_pops_file="${prefix}_nonsyn_pops_file.txt"
+syn_easySFS_outdir="${prefix}_easySFS_output_syn/"
+nonsyn_easySFS_outdir="${prefix}_easySFS_output_nonsyn/"
+num_samples=$(($num_ind * 2))
+
+# Merge all `.vcf` files.
+
+cp ${prefix}_chrom_1.vcf ${prefix}.vcf
+
+for i in {2..80}
+do
+  sed -i -r "s/^1\t/${i}\t/g" ${prefix}_chrom_${i}.vcf
+  grep ";MT=1\|;MT=2" ${prefix}_chrom_${i}.vcf >> ${prefix}.vcf
+done
+
+# Separate input `.vcf` file into synonymous and nonsynonymous.
+grep "#\|;MT=1" ${prefix}.vcf > $syn_inputvcf
+grep "#\|;MT=2" ${prefix}.vcf > $nonsyn_inputvcf
+# Write pops_file.txt for easySFS.py.
+python write_pops_file.py $num_ind 0 ${prefix}_syn
+python write_pops_file.py $num_ind 0 ${prefix}_nonsyn
+# Use easySFS.py to compute synonymous and nonsynonymous sfs.
+python easySFS.py -i $syn_inputvcf -p $syn_output_pops_file -o $syn_easySFS_outdir -f -a --proj $easySFS_proj
+python easySFS.py -i $nonsyn_inputvcf -p $nonsyn_output_pops_file -o $nonsyn_easySFS_outdir -f -a --proj $easySFS_proj
+# Use fitdadi_infer_DFE.py to infer DFE for given synonymous and nonsynonymous sfs.
+# python fitdadi_infer_DFE.py ${syn_easySFS_outdir}dadi/pop1.sfs ${nonsyn_easySFS_outdir}dadi/pop1.sfs ${prefix}_fitdadi_output/ --breed "LB"
+
+# INPUT ARGUMENTS
+prefix="../Data/no_demo_array/200_seed_${seed}" # Output prefix, and input prefix of given vcf
+
+# DERIVED ARGUMENTS
+inputvcf="${prefix}.vcf"
+syn_inputvcf="${prefix}_syn.vcf"
+nonsyn_inputvcf="${prefix}_nonsyn.vcf"
+syn_output_pops_file="${prefix}_syn_pops_file.txt"
+nonsyn_output_pops_file="${prefix}_nonsyn_pops_file.txt"
+syn_easySFS_outdir="${prefix}_easySFS_output_syn/"
+nonsyn_easySFS_outdir="${prefix}_easySFS_output_nonsyn/"
+num_samples=$(($num_ind * 2))
+
+# Merge all `.vcf` files.
+
+cp ${prefix}_chrom_1.vcf ${prefix}.vcf
+
+for i in {2..80}
+do
+  sed -i -r "s/^1\t/${i}\t/g" ${prefix}_chrom_${i}.vcf
+  grep ";MT=1\|;MT=2" ${prefix}_chrom_${i}.vcf >> ${prefix}.vcf
+done
+
+# Separate input `.vcf` file into synonymous and nonsynonymous.
+grep "#\|;MT=1" ${prefix}.vcf > $syn_inputvcf
+grep "#\|;MT=2" ${prefix}.vcf > $nonsyn_inputvcf
+# Write pops_file.txt for easySFS.py.
+python write_pops_file.py $num_ind 0 ${prefix}_syn
+python write_pops_file.py $num_ind 0 ${prefix}_nonsyn
+# Use easySFS.py to compute synonymous and nonsynonymous sfs.
+python easySFS.py -i $syn_inputvcf -p $syn_output_pops_file -o $syn_easySFS_outdir -f -a --proj $easySFS_proj
+python easySFS.py -i $nonsyn_inputvcf -p $nonsyn_output_pops_file -o $nonsyn_easySFS_outdir -f -a --proj $easySFS_proj
+# Use fitdadi_infer_DFE.py to infer DFE for given synonymous and nonsynonymous sfs.
+# python fitdadi_infer_DFE.py ${syn_easySFS_outdir}dadi/pop1.sfs ${nonsyn_easySFS_outdir}dadi/pop1.sfs ${prefix}_fitdadi_output/ --breed "LB"
+
+# INPUT ARGUMENTS
+prefix="../Data/no_demo_array/250_seed_${seed}" # Output prefix, and input prefix of given vcf
+
+# DERIVED ARGUMENTS
+inputvcf="${prefix}.vcf"
+syn_inputvcf="${prefix}_syn.vcf"
+nonsyn_inputvcf="${prefix}_nonsyn.vcf"
+syn_output_pops_file="${prefix}_syn_pops_file.txt"
+nonsyn_output_pops_file="${prefix}_nonsyn_pops_file.txt"
+syn_easySFS_outdir="${prefix}_easySFS_output_syn/"
+nonsyn_easySFS_outdir="${prefix}_easySFS_output_nonsyn/"
+num_samples=$(($num_ind * 2))
+
+# Merge all `.vcf` files.
+
+cp ${prefix}_chrom_1.vcf ${prefix}.vcf
+
+for i in {2..80}
+do
+  sed -i -r "s/^1\t/${i}\t/g" ${prefix}_chrom_${i}.vcf
+  grep ";MT=1\|;MT=2" ${prefix}_chrom_${i}.vcf >> ${prefix}.vcf
+done
+
+# Separate input `.vcf` file into synonymous and nonsynonymous.
+grep "#\|;MT=1" ${prefix}.vcf > $syn_inputvcf
+grep "#\|;MT=2" ${prefix}.vcf > $nonsyn_inputvcf
+# Write pops_file.txt for easySFS.py.
+python write_pops_file.py $num_ind 0 ${prefix}_syn
+python write_pops_file.py $num_ind 0 ${prefix}_nonsyn
+# Use easySFS.py to compute synonymous and nonsynonymous sfs.
+python easySFS.py -i $syn_inputvcf -p $syn_output_pops_file -o $syn_easySFS_outdir -f -a --proj $easySFS_proj
+python easySFS.py -i $nonsyn_inputvcf -p $nonsyn_output_pops_file -o $nonsyn_easySFS_outdir -f -a --proj $easySFS_proj
+# Use fitdadi_infer_DFE.py to infer DFE for given synonymous and nonsynonymous sfs.
+# python fitdadi_infer_DFE.py ${syn_easySFS_outdir}dadi/pop1.sfs ${nonsyn_easySFS_outdir}dadi/pop1.sfs ${prefix}_fitdadi_output/ --breed "LB"
+
+# INPUT ARGUMENTS
+prefix="../Data/no_demo_array/300_seed_${seed}" # Output prefix, and input prefix of given vcf
+
+# DERIVED ARGUMENTS
+inputvcf="${prefix}.vcf"
+syn_inputvcf="${prefix}_syn.vcf"
+nonsyn_inputvcf="${prefix}_nonsyn.vcf"
+syn_output_pops_file="${prefix}_syn_pops_file.txt"
+nonsyn_output_pops_file="${prefix}_nonsyn_pops_file.txt"
+syn_easySFS_outdir="${prefix}_easySFS_output_syn/"
+nonsyn_easySFS_outdir="${prefix}_easySFS_output_nonsyn/"
+num_samples=$(($num_ind * 2))
+
+# Merge all `.vcf` files.
+
+cp ${prefix}_chrom_1.vcf ${prefix}.vcf
+
+for i in {2..80}
+do
+  sed -i -r "s/^1\t/${i}\t/g" ${prefix}_chrom_${i}.vcf
+  grep ";MT=1\|;MT=2" ${prefix}_chrom_${i}.vcf >> ${prefix}.vcf
+done
+
+# Separate input `.vcf` file into synonymous and nonsynonymous.
+grep "#\|;MT=1" ${prefix}.vcf > $syn_inputvcf
+grep "#\|;MT=2" ${prefix}.vcf > $nonsyn_inputvcf
+# Write pops_file.txt for easySFS.py.
+python write_pops_file.py $num_ind 0 ${prefix}_syn
+python write_pops_file.py $num_ind 0 ${prefix}_nonsyn
+# Use easySFS.py to compute synonymous and nonsynonymous sfs.
+python easySFS.py -i $syn_inputvcf -p $syn_output_pops_file -o $syn_easySFS_outdir -f -a --proj $easySFS_proj
+python easySFS.py -i $nonsyn_inputvcf -p $nonsyn_output_pops_file -o $nonsyn_easySFS_outdir -f -a --proj $easySFS_proj
+# Use fitdadi_infer_DFE.py to infer DFE for given synonymous and nonsynonymous sfs.
+# python fitdadi_infer_DFE.py ${syn_easySFS_outdir}dadi/pop1.sfs ${nonsyn_easySFS_outdir}dadi/pop1.sfs ${prefix}_fitdadi_output/ --breed "LB"
