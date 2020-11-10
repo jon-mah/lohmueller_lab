53d9b9b2b215f8e6849bba9cc2a27ea526e62d32
Jonathan Mah <jonmah@uw.edu>
2020-11-10 07:47:19 -0800

    neutral shift test.

---
+++ Scripts/pop_2000_working_neutral_shift.bash
@@ -0,0 +1,29 @@
+#!/bin/bash
+#$ -cwd
+#$ -V
+#$ -m a
+#$ -l h_data=10G
+#$ -l h_rt=2:00:00
+
+# INPUT ARGUMENTS
+seed=1
+num_ind=8 # Number of samples from single population.
+prefix="../Data/pop_2000_working_neutral_shift/seed_${seed}" # Output prefix, and input prefix of given vcf
+easySFS_proj=16 # Number of chromosomes that sample is projected down into.
+
+# DERIVED Arguments
+inputvcf="${prefix}.vcf"
+syn_inputvcf="${prefix}_syn.vcf"
+nonsyn_inputvcf="${prefix}_nonsyn.vcf"
+syn_output_pops_file="${prefix}_syn_pops_file.txt"
+nonsyn_output_pops_file="${prefix}_nonsyn_pops_file.txt"
+syn_easySFS_outdir="${prefix}_easySFS_output_syn/"
+nonsyn_easySFS_outdir="${prefix}_easySFS_output_nonsyn/"
+num_samples=$(($num_ind * 2))
+
+# If `SLiM` is not an executable, then comment out the next line.
+slim pop_2000_working_neutral_shift_simulation
+
+# If `SLiM` is not an executable, then uncomment the next line and
+# provide a path to `SLiM`.
+# ./slim -d chrom=$SGE_TASK_ID -d init_seed=$seed pop_2000_working_neutral_shift_simulation.slim
