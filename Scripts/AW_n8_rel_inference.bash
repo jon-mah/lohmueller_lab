6be5cc661a29fd991132c23507def19bc62c21af
jon-mah <jonmah@uw.edu>
2021-01-11 08:41:10 -0800

    pipeline results.

--- Scripts/AW_n8_rel_inference.bash
+++ Scripts/AW_n8_rel_inference.bash
@@ -8,4 +8,4 @@
 # This script infers the demography and DFE of arctic wolves, given
 # their synonymous and nonsynonymous sfs.
 
-python fitdadi_infer_DFE.py ../Data/20201208_eduardo_sfs/AW.n8.S.rel.sfs ../Data/20201208_eduardo_sfs/AW.n8.NS.rel.sfs ../20201208_eduardo_sfs/AW_n8_rel/ --breed "AW"
+python fitdadi_infer_DFE.py ../Data/20201208_eduardo_sfs/AW.n8.S.rel.sfs ../Data/20201208_eduardo_sfs/AW.n8.NS.rel.sfs ../Data/20201208_eduardo_sfs/AW_n8_rel/ --breed "AW"
