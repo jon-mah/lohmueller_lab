861e3ddf66aed152e76396e83d8ebd662e634a4e
Jonathan Mah <jonmah@uw.edu>
2021-01-11 08:21:09 -0800

    param change for test.

--- Scripts/fitdadi_infer_DFE.py
+++ Scripts/fitdadi_infer_DFE.py
@@ -361,7 +361,7 @@ class DemographicAndDFEInference():
         with open(inferred_demography, 'w') as f:
             f.write('Beginning with demographic inference.\n')
             max_likelihood = -1e25
-            for i in range(25):
+            for i in range(5):
                 # Start at initial guess
                 p0 = initial_guess
                 # Randomly perturb parameters before optimization.
@@ -449,7 +449,7 @@ class DemographicAndDFEInference():
 
         gamma_max_likelihoods = []
         gamma_guesses = dict()
-        for i in range(25):
+        for i in range(5):
             p0 = initial_guess
             p0 = dadi.Misc.perturb_params(p0, lower_bound=lower_bound,
                                           upper_bound=upper_bound)
@@ -474,7 +474,7 @@ class DemographicAndDFEInference():
         upper_bound = [1, 1, upper_beta]
         neugamma_max_likelihoods = []
         neugamma_guesses = dict()
-        for i in range(25):
+        for i in range(5):
             p0_neugamma = initial_guess
             p0_neugamma = dadi.Misc.perturb_params(p0_neugamma,
                                                    lower_bound=lower_bound,
@@ -531,7 +531,7 @@ class DemographicAndDFEInference():
         with open(inferred_DFE, 'w') as f:
             f.write('Assuming a gamma-distributed DFE...\n')
             f.write('Outputting best 25 MLE estimates.\n')
-            for i in range(25):
+            for i in range(5):
                 best_popt = gamma_guesses[gamma_max_likelihoods[i]]
                 expected_sfs = spectra.integrate(
                     best_popt[1], self.gamma_dist, theta_nonsyn)
@@ -547,7 +547,7 @@ class DemographicAndDFEInference():
                 f.write('The expected SFS is: {0}.\n\n'.format(expected_sfs))
             f.write('Assuming a neutral-gamma-distributed DFE...\n')
             f.write('Outputting best 25 MLE estimates.\n')
-            for i in range(25):
+            for i in range(5):
                 best_popt_neugamma = neugamma_guesses[
                     neugamma_max_likelihoods[i]]
                 expected_sfs_neugamma = spectra.integrate(
